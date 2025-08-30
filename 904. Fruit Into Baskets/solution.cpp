8b30790a11a854caec57d89b85e571e4ceaa6af1
Achilles80 <adithyaajay2005@gmail.com>
2025-08-30 12:49:53 +0530

    Solved 904. Fruit Into Baskets

---
+++ 904. Fruit Into Baskets/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        map<int,int> mpp;
+        int l=0;
+        int r=0;
+        int maxi=0;
+        while(r<fruits.size()){
+            mpp[fruits[r]]++;
+            if(mpp.size()>2){
+                mpp[fruits[l]]--;
+                if(mpp[fruits[l]]==0) mpp.erase(fruits[l]);
+                l++;
+            }
+            if(mpp.size()<=2) maxi=max(maxi,r-l+1);
+            r++;
+        }
+        return maxi;
+    }
+};
