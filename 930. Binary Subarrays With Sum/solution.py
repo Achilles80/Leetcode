f09d0b83f90db75498dcb6648ad3d30790afe1b7
Achilles80 <adithyaajay2005@gmail.com>
2025-08-31 12:21:36 +0530

    Solved 930. Binary Subarrays With Sum

---
+++ 930. Binary Subarrays With Sum/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def helper(self,nums,goal):
+        r=0
+        l=0
+        sum=0
+        count=0
+        if(goal<0):
+            return 0
+        while(r<len(nums)):
+            sum+=nums[r]
+            while(sum>goal):
+                sum-=nums[l]
+                l+=1
+            count+=(r-l+1)
+            r+=1
+        return count
+
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        return self.helper(nums,goal)-self.helper(nums,goal-1)
+        
